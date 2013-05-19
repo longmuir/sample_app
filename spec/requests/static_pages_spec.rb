0e131ef96dc102193b133994b75bce3a14542a45
Jamie Longmuir <longmuir@gmail.fake>
2013-05-18 22:46:41 -0400

    Make a basic User model (including secure passwords)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,14 +5,22 @@ describe "StaticPages" do
 	let(:base_title) {"Ruby on Rails Tutorial Sample App" }
 
   subject { page }
+  #Refactoring from Chapter 5, exercise 1:
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1',     text: heading) }
+    it { should have_selector('title', text: full_title(page_title)) }
+  end
 
   describe "Home page" do
     before { visit root_path }
+    let (:heading) { 'Sample App' }
+    let (:page_title) { '' }
 
-    it { should have_selector('h1', text: 'Sample App') }
-    it { should have_selector('title', text: full_title('')) }
-    it { should_not have_selector('title', text: '| Home') }
+    it_should_behave_like "all static pages"
+    it { should_not have_selector 'title', text:'| Home' }
   end
+
+  #Refactoring from Chapter 5.3.4
 
   describe "Help page" do
     before { visit help_path }
@@ -21,7 +29,7 @@ describe "StaticPages" do
   	it { should have_selector('title', text: full_title('Help')) }
   end
 
-  # Note the first two methods have been refactored..
+  # Note the first two describe blocks have been refactored..
   # The second two have been left unfactored for educational purposees
   # Note: rspect automatically includes files in the 'support' folder.
 
@@ -51,6 +59,21 @@ describe "StaticPages" do
   		page.should have_selector('title',
   					  			:text => "#{base_title} | Contact")
   	end
+  end
+
+  it "sould have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', text: full_title('About Us')
+    click_link "Help"
+    page.should have_selector 'title', text: full_title('Help')
+    click_link "Contact"
+    page.should have_selector 'title', text: full_title('Contact')
+    click_link "Home"
+    click_link "Sign up now!"
+    page.should have_selector 'title', text: full_title('Sign up')
+    click_link "sample app"
+    page.should have_selector 'title', text: full_title('')
   end
 
 end
