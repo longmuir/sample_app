b6fbeb0dafb7f45ba9bcf5cfebf4ba9e82e9fb34
Jamie Longmuir <longmuir@gmail.fake>
2013-05-21 21:38:06 -0400

    Finish user signup

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -55,6 +55,16 @@ describe User do
   	end
   end
 
+  describe "email address with mixed case" do
+  	let(:mixed_case_email) { "Foo@ExAmPle.coM" }
+
+  	it "should be saved as all lower-case" do
+  		@user.email = mixed_case_email
+  		@user.save
+  		@user.reload.email.should == mixed_case_email.downcase
+  	end
+  end
+
   describe "when email format is valid" do
   	it "should be valid" do
   		addresses = %w[user@foo.COM A_US-ER@f.b.org frest.lst@foo.jp a+b@bax.com]
